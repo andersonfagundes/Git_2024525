714431704636902849f67d8430306623741aae5a
anderson <anderson@laptop>
2024-11-29 12:17:02 +0000

    Printing my name and my student number

--- src/git_2024525/Git_2024525.java
+++ src/git_2024525/Git_2024525.java
@@ -14,7 +14,8 @@ public class Git_2024525 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // printing my name and my student number
+        System.out.println("Anderson Fagundes Santos - 2024525");
     }
     
 }
